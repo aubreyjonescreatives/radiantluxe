bb8c70b917bad1cb7fb3bbc6feff35f3eae37dc2
Aubrey Jones <aubrey_jones07@hotmail.com>
2024-01-04 20:09:56 -0700

    added vue

---
+++ JS/index.js
@@ -0,0 +1,14 @@
+
+
+
+
+  const { createApp, ref } = Vue
+
+  createApp({
+    setup() {
+      const message = ref('Hello vue!')
+      return {
+        message
+      }
+    }
+  }).mount('#app')
